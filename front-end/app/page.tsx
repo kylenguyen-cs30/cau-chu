ed8c2c8d5714031df9ddd4762c1598266e0b25c5
Kyle Nguyen <knguyen1193@outlook.com>
2024-07-30 22:32:05 -0700

    fix front-end/ and back-end/

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,41 +1,71 @@
 "use client";
-
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
+interface Pet {
+  imageFilename: string;
+  name: string;
+  type: string;
+  breed: string;
+  age: number;
+  price: number;
+}
+
 export default function Home() {
-  const [pets, setPets] = useState([]);
+  const [pets, setPets] = useState<Pet[]>([]);
   // fetch pets from back end
   useEffect(() => {
     fetch("http://127.0.0.1:5000/pets")
-      .then((res) => res.json())
-      .then((data) => setPets(data))
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not okay");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Fetched data", data);
+        setPets(data);
+      })
       .catch((error) => console.error("Error fetching pets:", error));
   }, []);
+
+  console.log(pets);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <header className="text-center p-4 text-3xl font-bold text-white bg-gray-80">
+      <header className="text-center p-4 text-3xl font-bold text-black bg-gray-80">
         Welcome to Cậu Chủ Shop
       </header>
       <div className="flex flex-grow grid grid-cols-3 gap-4 p-4">
-        {pets.map((pet, index) => (
-          <div key={index} className="card bg-white shadow-lg rounded p-4">
-            <img
-              src={`/images/pets/${pet.imageFileName}`}
-              alt={pet.name}
-              className="h-48 w-full object-cover rounded"
-            />
-            <p>Type: {pet.type}</p>
-            <p>Breed: {pet.breed}</p>
-            <p>Age: {pet.age} years</p>
-            <p className="font-bold">Price: ${pet.price}</p>
-            <button
-              className="mt-4 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => alert("Added to cart!")}
+        {pets.length > 0 &&
+          pets.map((pet, index) => (
+            <div
+              key={index}
+              className="card bg-white shadow-lg rounded p-4 text-black"
             >
-              Add to Cart
-            </button>
-          </div>
-        ))}
+              {pet.imageFilename ? (
+                <Image
+                  src={`/images/pets/${pet.imageFilename}`}
+                  alt={pet.name}
+                  width={500}
+                  height={300}
+                  className="object-cover rounded"
+                />
+              ) : (
+                <div>No Image Available</div> // Placeholder in case of no image
+              )}
+              <p>Type: {pet.type}</p>
+              <p>Breed: {pet.breed}</p>
+              <p>Age: {pet.age} years</p>
+              <p className="font-bold">Price: ${pet.price}</p>
+              <button
+                className="mt-4 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => alert("Added to cart!")}
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
       </div>
       <footer className="text-center p-4 text-white bg-gray-800">
         {" "}
