e90822ebb53c75308f4c807d4a4a042d96c030dd
Kyle Nguyen <knguyen1193@outlook.com>
2024-08-19 14:06:38 -0700

    complete migration to metadata

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+// import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,21 +11,6 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
-  // links: [
-  //   {
-  //     rel: "preconnect",
-  //     href: "https://fonts.googleapis.com",
-  //   },
-  //   {
-  //     rel: "preconnect",
-  //     href: "https://fonts.gstatic.com",
-  //     crossOrigin: "true",
-  //   },
-  //   {
-  //     rel: "stylesheet",
-  //     href: "https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap",
-  //   },
-  // ],
 };
 
 export default function RootLayout({
@@ -35,20 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        {" "}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
