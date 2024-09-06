b6b026a04c5680ee4d4dc32fd02f9e13bf6e1850
Kyle Nguyen <knguyen1193@outlook.com>
2024-09-05 17:41:16 -0700

    still fixing

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["http://localhost:3000"])
+    # CORS(app, origins=["http://localhost:3000"])
     # CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:3000"}})
 
     # CORS(app, origins="*")
